79bb296a1713e0d5e3a5f6057d840c8cf83cf735
ashish patil <patilas@visa.com>
2015-05-20 20:26:47 +0100

    refactor code with spacing and modify the way resource is acquired

--- src/main/java/com/ace/ReportCreator.java
+++ src/main/java/com/ace/ReportCreator.java
@@ -10,17 +10,19 @@ import java.util.HashMap;
 public class ReportCreator {
 
     public String createPDFReport() {
-        Connection conn = null;
         String fileName = "Students-Report.pdf";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/college", "ash", "password");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/college", "ash", "password");
+
+
+            InputStream inputStream = getClass().getResourceAsStream("/school.xml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
 
             HashMap<String, Object> jasperParameter = new HashMap<String, Object>();
             jasperParameter.put("QUERY", "select * from students");
-            InputStream inputStream = ReportClient.class.getResourceAsStream("/school.xml");
-            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,jasperParameter, conn);
             JasperExportManager.exportReportToPdfFile(jasperPrint, fileName);
 
