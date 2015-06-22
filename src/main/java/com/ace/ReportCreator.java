15eafe878688f79b5e2c40e917e4112215719f13
ashish patil <patilas@visa.com>
2015-06-22 17:08:22 +0100

    refactor code and add xls creation code

--- src/main/java/com/ace/ReportCreator.java
+++ src/main/java/com/ace/ReportCreator.java
@@ -1,6 +1,8 @@
 package com.ace;
 
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 
 import java.io.InputStream;
 import java.sql.Connection;
@@ -12,24 +14,38 @@ public class ReportCreator {
     public String createPDFReport() {
 
         try {
-            Class.forName(Constant.DB_DRIVER.getValue());
-            Connection conn = DriverManager.getConnection(Constant.DB_URL.getValue(), Constant.USER.getValue(), Constant.PASSWORD.getValue());
+            Class.forName(Constants.DB_DRIVER);
+            Connection conn = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASSWORD);
 
 
-            InputStream inputStream = getClass().getResourceAsStream(Constant.REPORT_XML.getValue());
+            InputStream inputStream = getClass().getResourceAsStream(Constants.REPORT_XML);
             JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
 
             HashMap<String, Object> jasperParameter = new HashMap<String, Object>();
-            jasperParameter.put(Constant.QUERY.getValue(), Constant.QUERY_VALUE.getValue());
-            jasperParameter.put(Constant.REPORT_NAME.getValue(), Constant.REPORT_NAME_VALUE.getValue());
+            jasperParameter.put(Constants.QUERY, Constants.QUERY_VALUE);
+            jasperParameter.put(Constants.REPORT_NAME, Constants.REPORT_NAME_VALUE);
+
 
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,jasperParameter, conn);
-            JasperExportManager.exportReportToPdfFile(jasperPrint, Constant.FILE_NAME.getValue());
+            JasperExportManager.exportReportToPdfFile(jasperPrint, Constants.FILE_NAME + Constants.PDF_EXT);
+
+            JRXlsxExporter exporter = new JRXlsxExporter();
+            exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, Constants.FILE_NAME + Constants.XLS_EXT);
+            exporter.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
+            exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_COLUMNS, Boolean.TRUE);
+            exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
+            exporter.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.FALSE);
+            exporter.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.FALSE);
+            exporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+            exporter.setParameter(JRXlsExporterParameter.IS_IGNORE_GRAPHICS, Boolean.TRUE);
+            exporter.exportReport();
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Constant.FILE_NAME.getValue();
+        return Constants.FILE_NAME + Constants.PDF_EXT;
 
     }
 
