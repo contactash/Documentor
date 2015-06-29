7bc3d004e669d9efd5035fccf156e24088ccf2d4
ashish patil <patilas@visa.com>
2015-06-29 09:36:12 +0100

    remove spaces

--- src/main/java/com/ace/template/ReportTemplate.java
+++ src/main/java/com/ace/template/ReportTemplate.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
 
 //This is interface is so that we can create different types of report template like
 // product, sales, order etc
-
 public interface ReportTemplate {
 
     InputStream getReportTemplate();
+
     String getFileName();
 
     HashMap<String,Object> setReportParameters();
