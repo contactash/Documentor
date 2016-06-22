e974e7c86e6e6d005f5d31c8ca6e13d10324c18e
ashish patil <patilas@visa.com>
2016-06-22 06:30:55 +0100

    open in file and application

---
+++ src/test/java/com/ace/connection/impl/MySQLConnectionTest.java
@@ -0,0 +1,54 @@
+package com.ace.connection.impl;
+
+import com.ace.connection.Connections;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MySQLConnectionTest {
+    private Connections mysqlConnection;
+    @Mock Connection mockConnection;
+
+    @Before
+    public void setUp() {
+        //setup
+        mysqlConnection = new MySQLConnection();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldCreateConnection() throws Exception {
+        //execution
+        Connection actualConnection = mysqlConnection.getConnection();
+        //verification
+        assertThat(actualConnection, is(notNullValue()));
+
+    }
+
+    @Test
+    public void shouldCloseConnection() throws Exception {
+        mysqlConnection.closeConnection(mysqlConnection.getConnection());
+        verify(mockConnection, times(1));
+    }
+
+    @After
+    public void tearDown() throws SQLException, ClassNotFoundException {
+        mysqlConnection.closeConnection(mysqlConnection.getConnection());
+        mysqlConnection = null;
+    }
+
+}
