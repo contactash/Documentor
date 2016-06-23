e60b572d06d24f0dbe0eefb8f3ded57dfb4bf699
ash.pat <contactashpat@gmail.com>
2016-06-23 14:24:35 +0100

    Add unit test

--- src/test/java/com/ace/utils/MySQLConnectionUtilsTest.java
+++ src/test/java/com/ace/utils/MySQLConnectionUtilsTest.java
@@ -4,29 +4,26 @@ import com.ace.connection.impl.MySQLConnection;
 import com.mysql.jdbc.Connection;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MySQLConnectionUtilsTest {
 
-    @Mock MySQLConnection mockMySQLConnection;
-    @Mock Connection mockConnection;
-    private MySQLConnection mySQLConnection = new MySQLConnection();
+    private MySQLConnection mySQLConnection;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        mySQLConnection = new MySQLConnection();
     }
 
     @Test
     public void testGetConnection() throws Exception {
-        Mockito.when(mockMySQLConnection.getConnection()).thenReturn(mockConnection);
-        Connection result = (Connection) mySQLConnection .getConnection();
-        assertThat(result, is(mockConnection));
+        Connection actualResult = (Connection) mySQLConnection .getConnection();
+        assertThat(actualResult, is(instanceOf(Connection.class)));
     }
 
 }
