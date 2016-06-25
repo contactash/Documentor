7607e23b8d00bc565e50713c0527e875d09a0885
ashish patil <patilas@visa.com>
2016-06-25 10:14:44 +0100

    Make test work with change in junit version. Use power mockito to test static

--- src/test/java/com/ace/utils/MySQLConnectionUtilsTest.java
+++ src/test/java/com/ace/utils/MySQLConnectionUtilsTest.java
@@ -8,7 +8,7 @@ import org.mockito.MockitoAnnotations;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
 
 public class MySQLConnectionUtilsTest {
 
